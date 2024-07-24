0507fd9122279a967f4ff437ec7c131691d05764
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-07-24 18:37:39 -0400

    add plots

--- predict_timegtp.py
+++
@@ -1,8 +0,0 @@
-from nixtla import NixtlaClient
-
-nixtla_client = NixtlaClient(
-    api_key = 'nixtla-tok-ltLeWIWTCxxTpIdEMPBE6fCtJNTEoaqvzGwNmR6LceJTdFhbnAVT50fdxJ02yZTCvAqzsCmx0vaYBoYx'
-)
-
-
-nixtla_client.validate_api_key()
---
+++ test_API.py
@@ -0,0 +1,17 @@
+from nixtla import NixtlaClient
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Asegúrate de que tu API key sea correcta
+nixtla_client = NixtlaClient(
+    api_key='nixtla-tok-ltLeWIWTCxxTpIdEMPBE6fCtJNTEoaqvzGwNmR6LceJTdFhbnAVT50fdxJ02yZTCvAqzsCmx0vaYBoYx'
+)
+
+# Cargar los datos
+df = pd.read_csv('https://raw.githubusercontent.com/Nixtla/transfer-learning-time-series/main/datasets/air_passengers.csv')
+
+# Mostrar las primeras filas del DataFrame
+print(df.head())
+
+# Crear un gráfico usando NixtlaClient
+nixtla_client.plot(df, time_col='timestamp', target_col='value')
