6c5ac1de94bcefcca83e4173cb400a6784d2f53e
felipe alvarez <fe.alvarezdiaz@gmail.com>
2024-07-24 20:36:03 -0400

    get the data

--- test_API.py
+++ test_API.py
@@ -18,41 +18,4 @@ print(df.head())
 nixtla_client.plot(df, time_col='timestamp', target_col='value')
 
 timegpt_fcst_df = nixtla_client.forecast(df=df, h=12, freq='MS', time_col='timestamp', target_col='value')
-timegpt_fcst_df.head()
-
-
-
-
-# obtener datos de coingecko
-url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
-params = {
-    'vs_currency': 'usd',
-    'days': 'max'  # Puedes cambiar 'max' por el número de días que necesitas
-}
-
-# Hacer la solicitud a la API
-response = requests.get(url, params=params)
-
-# Verificar que la solicitud fue exitosa
-if response.status_code == 200:
-    data = response.json()
-else:
-    print("Error:", response.status_code)
-    data = None
-
-# Convertir los datos a un DataFrame de pandas
-if data:
-    prices = data['prices']
-    df = pd.DataFrame(prices, columns=['timestamp', 'price'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-
-    # Mostrar las primeras filas del DataFrame
-    print(df.head())
-
-    # Crear una gráfica de los datos
-    plt.figure(figsize=(10, 5))
-    plt.plot(df['timestamp'], df['price'])
-    plt.xlabel('Fecha')
-    plt.ylabel('Precio (USD)')
-    plt.title('Precio de Bitcoin')
-    plt.show()
+print(timegpt_fcst_df.head())
